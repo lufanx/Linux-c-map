97f525b63bd52cff8a45ecf90057a5a8379b2103
lu fan <lux.fan@intel.com>
2017-12-26 10:34:10 +0800

    c_isspace.c: add this .c file.
    This .c file is check char whether is space char.
    
    Signed-off-by: lu fan <lux.fan@intel.com>

---
+++ char/c_isspace.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char *argv)
+{
+	char *buf = "ab 87 ko\tm";
+	int i = 0;
+
+	while (buf[i] != '\0') {
+		if (isspace(buf[i])) {
+			printf("buf[%d] is space\n", i);
+		}
+		i++;
+	}
+
+	return 0;
+}
