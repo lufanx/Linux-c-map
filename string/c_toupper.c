9e927b4306b10496dc434efda0ca97792127339d
lu fan <lux.fan@intel.com>
2017-12-27 12:32:52 +0800

    string/c_tolower.c string/c_toupper.c: add this 2 .c file.
    tolower() will upper transport lower, toupper() will lower transport upper.
    
    Signed-off-by: lu fan <lux.fan@intel.com>

---
+++ string/c_toupper.c
@@ -0,0 +1,17 @@
+#include "../include/stdinc.h"
+
+int
+main(int argc, char *argv[])
+{
+	char *buf = "BHlpohYt";
+	char dst[9];
+	int i = 0;
+
+	memset(dst, 0, sizeof(dst));
+	while (buf[i] != '\0') {
+		dst[i] = toupper(buf[i]);
+		i++;
+	}
+	printf("dst = %s\n", dst);
+	return 0;
+}
