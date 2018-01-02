f9623e59bc39798e57fc7e65ac4d845535e1462c
lu fan <554001112@qq.com>
2018-01-02 11:34:30 +0800

    dd strcasecmp.c and strcat.c.
    Signed-off-by: lu fan <554001112@qq.com>

---
+++ string/c_strcat.c
@@ -0,0 +1,14 @@
+#include "../include/stdinc.h"
+
+int
+main(int argc, char *argv[])
+{
+	char *src = "abcd";
+	char dest[] = "efgh";
+
+	strcat(dest, src);
+	printf("%d %d\n", strlen(dest), sizeof(dest));
+
+	printf("dest = %s\n", dest);
+	return 0;
+}
