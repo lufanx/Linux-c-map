e862f5e0989c7035f7b80bd850c7d237e9b6764e
lufanx <554001112@qq.com>
2018-04-15 21:37:24 +0800

    add about create thread example.
    Signed-off-by: lufanx <554001112@qq.com>

---
+++ thread/creat_thread_2.c
@@ -0,0 +1,53 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct
+{
+	char name[20];
+	int time;
+	int start;
+	int end;
+
+}RaceArg;
+
+
+void *
+th_fn(void *arg)
+{
+	RaceArg *r = (RaceArg *)arg;
+	int i = r->start;
+
+	for (; i <= r->end; i++) {
+		printf ("%s(%lx) running %d\n", r->name, pthread_self(), i);
+		usleep(r->time);	
+	}
+
+	return (void *)0;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int err;
+	pthread_t  rabbit, turtle;
+	
+	RaceArg r_a = {"rabbit", (int)(drand48()*100000000), 20, 50};
+	RaceArg t_a = {"turtle", (int)(drand48()*100000000), 10, 60};
+
+	if ((err = pthread_create(&rabbit, NULL, th_fn, (void *)&r_a)) != 0) {
+		perror ("create failed!");
+	}
+	if ((err = pthread_create(&turtle, NULL, th_fn, (void *)&t_a)) != 0) {
+		perror ("create failed!");
+	}
+
+	pthread_join(rabbit, NULL);
+	pthread_join(turtle, NULL);
+
+	printf ("control thread is %lx\n", pthread_self());
+	printf ("finished\n");
+
+	return 0;
+}
